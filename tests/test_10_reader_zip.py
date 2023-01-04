0979af53c50af96b26c6469fb6770710394aa258
Eddy <Edward.Comyn-Platt@ecmwf.int>
2023-01-04 15:23:49 +0000

    some tests

--- tests/test_10_reader_zip.py
+++ tests/test_10_reader_zip.py
@@ -8,6 +8,9 @@ MULTI_NETCDF_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_multi_netcdf.zip
 MIXED_MULTI_NETCDF_ZIP_FILE = (
     f"{ROOT_DIR}/notebooks/data/ERA5_UK_mixed_multi_netcdf.zip"
 )
+MIXED_MULTI_NETCDF_NO_TIME_ZIP_FILE = (
+    f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_multi_netcdf_no_time_dim.zip"
+)
 SINGLE_GRIB_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_single_grib.zip"
 MULTI_CSV_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_multi_csv.zip"
 SINGLE_CSV_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_single_csv.zip"
@@ -20,6 +23,7 @@ def test_emohawk_open_zip():
         SINGLE_NETCDF_ZIP_FILE,
         MULTI_NETCDF_ZIP_FILE,
         MIXED_MULTI_NETCDF_ZIP_FILE,
+        MIXED_MULTI_NETCDF_NO_TIME_ZIP_FILE,
         SINGLE_CSV_ZIP_FILE,
         # NOT YET IMPLEMENTED:
         # MULTI_CSV_ZIP_FILE,
