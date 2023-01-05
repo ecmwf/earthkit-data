dd2d3b7b1648a84dffc390b5726bb158ef20d455
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-01-05 10:24:57 +0000

    Feature/handle incompatible multifile zip (#12)
    * handle zip files
    
    * Updates to directory reader
    
    * remove debug print statements
    
    * ignore subdirectories in data dir (i.e. created by unzipped test data)
    
    * added zip reader tests and data
    
    * mixed multi netcdf compatible
    
    * potential to handle incompatible contents of zip files
    
    * elif not required

--- tests/test_10_reader_zip.py
+++ tests/test_10_reader_zip.py
@@ -8,7 +8,10 @@ MULTI_NETCDF_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_multi_netcdf.zip
 MIXED_MULTI_NETCDF_ZIP_FILE = (
     f"{ROOT_DIR}/notebooks/data/ERA5_UK_mixed_multi_netcdf.zip"
 )
-MIXED_MULTI_NETCDF_NO_TIME_ZIP_FILE = (
+INCOMPATIBLE_MULTI_NETCDF_ZIP_FILE = (
+    f"{ROOT_DIR}/notebooks/data/ERA5_incompatible_mixed_multi_netcdf.zip"
+)
+MULTI_NETCDF_NO_TIME_ZIP_FILE = (
     f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_multi_netcdf_no_time_dim.zip"
 )
 SINGLE_GRIB_ZIP_FILE = f"{ROOT_DIR}/notebooks/data/ERA5_t2m_UK_single_grib.zip"
@@ -22,8 +25,9 @@ def test_emohawk_open_zip():
     for filename in [
         SINGLE_NETCDF_ZIP_FILE,
         MULTI_NETCDF_ZIP_FILE,
+        MULTI_NETCDF_NO_TIME_ZIP_FILE,
         MIXED_MULTI_NETCDF_ZIP_FILE,
-        MIXED_MULTI_NETCDF_NO_TIME_ZIP_FILE,
+        INCOMPATIBLE_MULTI_NETCDF_ZIP_FILE,
         SINGLE_CSV_ZIP_FILE,
         # NOT YET IMPLEMENTED:
         # MULTI_CSV_ZIP_FILE,
