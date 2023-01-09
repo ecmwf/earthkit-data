d8da943e1fb2108f3b91b5c2bab4662c19d04e73
James Varndell <36902610+JamesVarndell@users.noreply.github.com>
2023-01-09 14:56:04 +0000

    Makes geopandas an optional dependency (#10)
    * Makes geopandas an optional dependency
    
    * import geopandas when required
    
    Co-authored-by: Eddy <Edward.Comyn-Platt@ecmwf.int>

--- emohawk/readers/shapefile.py
+++ emohawk/readers/shapefile.py
@@ -6,9 +6,6 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-
-import geopandas as gpd
-
 from . import Reader
 
 
@@ -16,6 +13,9 @@ class ShapefileReader(Reader):
     """
     Class for reading and polymorphing ESRI Shapefiles.
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def to_pandas(self, **kwargs):
         """
@@ -25,6 +25,8 @@ class ShapefileReader(Reader):
         -------
         pandas.core.frame.DataFrame
         """
+        import geopandas as gpd
+
         return gpd.read_file(self.source, **kwargs)
 
     def to_json(self, file_name):
