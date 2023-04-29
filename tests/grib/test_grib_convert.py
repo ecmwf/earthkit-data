333791c3c31e5f1d9d7b9112552d2a338c0553ac
cosunae <carlos.osuna@env.ethz.ch>
2023-04-29 13:05:31 +0200

    add icon grib file for testing and test to convert to xarray

--- tests/grib/test_grib_convert.py
+++ tests/grib/test_grib_convert.py
@@ -9,49 +9,17 @@
 # nor does it submit to any jurisdiction.
 #
 
-import numpy as np
-
 from earthkit.data import from_source
 from earthkit.data.testing import earthkit_test_data_file
 
 
-def test_grib_to_pandas():
-    f = from_source("file", earthkit_test_data_file("test_single.grib"))
-
-    # all points
-    df = f.to_pandas()
-    assert len(df) == 84
-    cols = [
-        "lat",
-        "lon",
-        "value",
-        "datetime",
-        "domain",
-        "levtype",
-        "date",
-        "time",
-        "step",
-        "param",
-        "class",
-        "type",
-        "stream",
-        "expver",
-    ]
-    assert list(df.columns) == cols
-    assert np.allclose(df["lat"][0:2], [90, 90])
-    assert np.allclose(df["lon"][0:2], [0, 30])
-    assert np.allclose(df["value"][0:2], [260.435608, 260.435608])
-
-    # specific location
-    df = f.to_pandas(latitude=90, longitude=30)
-    assert len(df) == 1
-    assert list(df.columns) == cols
-    assert np.isclose(df["lat"][0], 90)
-    assert np.isclose(df["lon"][0], 30)
-    assert np.isclose(df["value"][0], 260.435608)
-
-
-if __name__ == "__main__":
-    from earthkit.data.testing import main
-
-    main()
+def test_icon_to_xarray():
+    # test the conversion to xarray for an icon (unstructured grid) grib file.
+    g = from_source("file", earthkit_test_data_file("test_icon.grib"))
+    ds = g.to_xarray()
+    assert len(ds.data_vars) == 1
+    # Dataset contains 9 levels and 9 grid points per level
+    ref_levs = g.metadata("level")
+    assert ds["pres"].sizes["generalVerticalLayer"] == len(ref_levs)
+    assert ds["pres"].sizes["values"] == 6
+    assert False
