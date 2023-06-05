64de739f86867ccd955e877de830aa31039860cf
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-06-05 07:48:48 +0000

    Translators and Wrappers (#76)
    * removing unused cruft config
    
    * removing unused cruft config
    
    * adding pyyaml dependancy
    
    * stuff
    
    * qa
    
    * wrappers and translators added
    
    * remove print statement
    
    * self aware xarray
    
    * wrapper/translators and tests
    
    * tests added
    
    * tests added
    
    * remove commented code
    
    * qa and test corrections
    
    * qa
    
    * from_object example notebook
    
    * from_object tests added
    
    * tests added and qa updates
    
    * using public method

---
+++ earthkit/data/translators/__init__.py
@@ -0,0 +1,49 @@
+import logging
+import os
+
+import earthkit.data as data
+from earthkit.data.core import Base
+from earthkit.data.decorators import locked
+from earthkit.data.wrappers import _helpers
+
+LOG = logging.getLogger(__name__)
+
+
+class Translator(Base):
+    def __call__(self):
+        """
+        Return unmodified data.
+        """
+        return self.data
+
+
+_TRANSLATORS = {}
+
+
+@locked
+def _translators():
+    return _helpers(
+        "translator", _TRANSLATORS, here=os.path.dirname(__file__), package=__name__
+    )
+
+
+def get_translator(source, cls, *args, **kwargs):
+    """
+    Get the appropriate translator for the source based on the target cls.
+    """
+    if not isinstance(source, Base):
+        source = data.from_object(source)
+
+    for name, h in _translators().items():
+        translator = h(source, cls, *args, **kwargs)
+        if translator is not None:
+            return translator
+
+    raise ValueError(f"Cannot find a translator for class {cls.__name__}")
+
+
+def transform(*args, **kwargs):
+    """
+    Executing wrapper for the get_translator class method
+    """
+    return get_translator(*args, **kwargs)()
---
+++ tests/wrappers/test_ndarray.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+import logging
+
+from earthkit.data import from_object, wrappers
+from earthkit.data.wrappers import ndarray as ndwrapper
+
+LOG = logging.getLogger(__name__)
+
+
+def test_ndarray_wrapper():
+    import numpy as np
+
+    _wrapper = ndwrapper.wrapper(np.array([]))
+    assert isinstance(_wrapper, ndwrapper.NumpyNDArrayWrapper)
+    _wrapper = wrappers.get_wrapper(np.array([]))
+    assert isinstance(_wrapper, ndwrapper.NumpyNDArrayWrapper)
+    _wrapper = from_object(np.array([]))
+    assert isinstance(_wrapper, ndwrapper.NumpyNDArrayWrapper)
