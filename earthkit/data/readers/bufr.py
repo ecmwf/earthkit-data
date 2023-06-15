9e2af49c7000f048aa421cb9ecc2cd6b4ed49df9
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2023-06-15 12:18:59 +0100

    Add dump method to bufr

--- earthkit/data/readers/bufr.py
+++ earthkit/data/readers/bufr.py
@@ -9,6 +9,11 @@
 
 # See:
 # https://github.com/ecmwf/pdbufr
+
+import threading
+
+import eccodes
+
 from . import Reader
 
 COLUMNS = ("latitude", "longitude", "data_datetime")
@@ -30,12 +35,102 @@ BUFR_LS_KEYS = {
 }
 
 
+class BUFRInOneFile:
+    def __init__(self, path):
+        self.path = path
+        self.lock = threading.Lock()
+        self.file = open(self.path, "rb")
+        self.offsets = []
+        self.lengths = []
+        self.num = None
+
+    def __del__(self):
+        try:
+            # print("CLOSE", self.path)
+            self.file.close()
+        except Exception:
+            pass
+
+    def _get_positions(self):
+        if not self.offsets:
+            with self.lock:
+                while True:
+                    pos = self.file.tell()
+                    handle = eccodes.codes_bufr_new_from_file(self.file)
+                    if handle is None:
+                        break
+                    self.offsets.append(pos)
+                    self.lengths.append(self.file.tell() - pos)
+                    eccodes.codes_release(handle)
+
+        if self.num is None:
+            self.num = len(self.offsets)
+
+    def __len__(self):
+        if self.num is None:
+            with self.lock:
+                self.num = eccodes.codes_count_in_file(self.file)
+            return self.num
+
+    def dump(self, n):
+        # self._get_positions()
+
+        # handle = self.at_offset(self.offsets[n])
+        # handle = self.at_offset(0)
+        import json
+        import warnings
+
+        import eccodes
+
+        from earthkit.data.core.temporary import temp_file
+
+        with temp_file() as filename:
+            # self._get_positions()
+            # handle = self.at_offset(self.offsets[n])
+            handle = self.at_offset(0)
+            eccodes.codes_set(handle, "unpack", 1)
+            with open(filename, "w") as f:
+                eccodes.codes_dump(handle, f, "json")
+
+            eccodes.codes_release(handle)
+
+            with open(filename, "r") as f:
+                try:
+                    d = json.loads(f.read())
+                    # print(d)
+                    return d
+                except Exception as e:
+                    warnings.warn("Failed to parse bufr_dump", e)
+                    return None
+
+    # def __getitem__(self, n):
+    #     elf.offsets[n], self.lengths[n])
+
+    def at_offset(self, offset):
+        with self.lock:
+            print(f"offsets={self.offsets} ")
+            print(f"path={self.path} offset={offset} file={self.file}")
+            print(f"tell={self.file.tell()}")
+            self.file.seek(offset, 0)
+            print(f"tell={self.file.tell()}")
+            handle = eccodes.codes_bufr_new_from_file(self.file)
+
+            # handle = eccodes.codes_new_from_file(
+            #     self.file,
+            #     eccodes.CODES_PRODUCT_BUFR,
+            # )
+            print(f"handle={handle}")
+            assert handle is not None
+            # return CodesHandle(handle, self.path, offset)
+            return handle
+
+
 class BUFRReader(Reader):
     """Represents a BUFR file"""
 
     def __init__(self, source, path):
         super().__init__(source, path)
-        self._num = None
+        self._reader = BUFRInOneFile(self.path)
 
     def to_pandas(self, columns=COLUMNS, filters=None, **kwargs):
         """Extracts BUFR data into an pandas DataFranme using :xref:`pdbufr`.
@@ -66,12 +161,7 @@ class BUFRReader(Reader):
         return pdbufr.read_bufr(self.path, columns=columns, filters=filters)
 
     def __len__(self):
-        if self._num is None:
-            import eccodes
-
-            with open(self.path, "rb") as f:
-                self._num = eccodes.codes_count_in_file(f)
-        return self._num
+        return len(self._reader)
 
     def ls(self, *args, **kwargs):
         from pdbufr.high_level_bufr.bufr import BufrFile
@@ -99,6 +189,37 @@ class BUFRReader(Reader):
                         break
 
         return ls(_proc, BUFR_LS_KEYS, *args, **kwargs)
+
+    def dump(self, index, **kwargs):
+        # import json
+        # import subprocess
+        # import sys
+        # import warnings
+        # from earthkit.data.core.temporary import temp_file
+        from earthkit.data.utils.dump import make_bufr_html_tree
+
+        d = self._reader.dump(index)
+        if d is not None:
+            return make_bufr_html_tree(d, **kwargs)
+
+        # with temp_file() as filename:
+        #     cmd = f"bufr_dump -js -w count=1 -X 0 {self.path} > {filename}"
+        #     try:
+        #         ret = subprocess.call(cmd, shell=True)
+        #         if ret < 0:
+        #             warnings.warn("Failed to generate bufr_dump", file=sys.stderr)
+        #             return None
+        #     except OSError as e:
+        #         warnings.warn("Failed to generate bufr_dump", e, file=sys.stderr)
+        #         return None
+
+        #     with open(filename, "r") as f:
+        #         try:
+        #             d = json.loads(f.read())
+        #             return make_bufr_html_tree(d, **kwargs)
+        #         except Exception as e:
+        #             warnings.warn("Failed to parse bufr_dump", e)
+        #             return None
 
 
 def reader(source, path, magic=None, deeper_check=False):
---
+++ earthkit/data/utils/dump.py
@@ -0,0 +1,177 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from earthkit.data.core.ipython import ipython_active
+
+
+class BUFRTree:
+    """Restructure the result of the bufr_dump ecCodes command."""
+
+    def make_tree(self, data):
+        """Restructure the the result of json bufr_dump into a format better
+        suited for generating a tree view out of it.
+
+        Parameters
+        ----------
+        data: dict
+            The result of the json bufr_dump loaded into a dict.
+
+        Returns
+        -------
+        dict
+        """
+        return self._load_dump(data)
+
+    def _load_dump(self, data):
+        r = {"header": [], "data": []}
+
+        # data = data["messages"][0]
+
+        for i, v in enumerate(data):
+            # print(v)
+            k = v["key"]
+            val = v["value"]
+            if isinstance(val, list):
+                val = "array"
+
+            r["header"].append({"key": k, "value": val})
+
+            if k == "unexpandedDescriptors":
+                self._parse_dump(data[i + 1], r["data"])
+                break
+        return r
+
+    def _parse_dump(self, data, parent):
+        arrayCnt = 0
+        keyCnt = 0
+        for v in data:
+            if isinstance(v, list):
+                arrayCnt += 1
+            else:
+                # print(v)
+                keyCnt += 1
+        # if arrayCnt > 1:
+        #     for i, v in enumerate(data):
+        #         print(i, v)
+        #     return
+
+        for i, v in enumerate(data):
+            if not isinstance(v, list):
+                # print(i, v["key"], arrayCnt)
+                parent.append(
+                    {
+                        "key": v["key"],
+                        "value": v["value"],
+                        "units": v.get("units", None),
+                    }
+                )
+            else:
+                # print("    arrayCnt=", arrayCnt)
+                if arrayCnt > 1:
+                    # print("->", v[0])
+                    # print("  ", v[1])
+                    item = []
+                    parent.append(item)
+                    self._parse_dump(v, item)
+                    # print(f"item={item}")
+                    # break
+                else:
+                    self._parse_dump(v, parent)
+
+
+class BUFRHtmlTree:
+    def make_html(self, data):
+        """Generates a html/css tree view from the input representing the
+        result of bufr_dump.
+
+        Parameters
+        ----------
+        data: dict
+
+        Returns
+        -------
+        str
+        """
+        return self._build(data)
+
+    def _build(self, data):
+        td1 = self._node(data["header"], [])
+        td2 = self._node(data["data"], [])
+
+        td = self._top_node("header", td1) + self._top_node("data", td2)
+        t = f"""
+        <ul class="tree">
+        <li>
+            {td}
+        </li>
+        </ul>
+        """
+        return t
+
+    def _leaf(self, k, v, units):
+        return f"""<li>{k}: {v} [{units}]</li>"""
+
+    def _top_node(self, name, v):
+        return f"""
+        <li>
+        <details>
+            <summary>{name}</summary>
+            <ul>
+            {v}
+            </ul>
+            </details>
+        </li>
+        """
+
+    def _start_node(self, k, v):
+        return f"""
+        <li>
+        <details>
+            <summary>{k}: {v}</summary>
+            <ul>
+        """
+
+    def _end_node(self):
+        return """</ul>
+            </details>
+        </li>
+        """
+
+    def _node(self, data, parent):
+        td = ""
+        if isinstance(data, list):
+            if parent:
+                td += self._start_node(data[0]["key"], data[0]["value"])
+            for v in data:
+                td += self._node(v, data)
+            if parent:
+                td += self._end_node()
+        else:
+            td += self._leaf(data["key"], data["value"], data.get("units", None))
+
+        return td
+
+
+def make_bufr_html_tree(data, **kwargs):
+    do_print = kwargs.pop("print", False)
+
+    tree = BUFRTree().make_tree(data)
+    if not do_print and ipython_active:
+        from IPython.display import HTML
+
+        from earthkit.data.utils.html import css
+
+        t = BUFRHtmlTree().make_html(tree)
+        style = css("tree")
+        return HTML(style + t)
+
+    if do_print:
+        print(tree)
+    else:
+        return tree
