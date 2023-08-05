1d1c70544dddb9c8c7b61a3038eed410d8873b52
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2023-08-05 12:31:11 +0100

    Add save to disk tests for numpy fieldlist

--- tests/sources/test_numpy_list.py
+++ tests/sources/test_numpy_list.py
@@ -10,11 +10,13 @@
 #
 
 import logging
+import os
 
 import numpy as np
 
 from earthkit.data import from_source
 from earthkit.data.core.fieldlist import FieldList
+from earthkit.data.core.temporary import temp_file
 from earthkit.data.testing import earthkit_examples_file
 
 LOG = logging.getLogger(__name__)
@@ -30,12 +32,22 @@ def test_numpy_list_grib_single_field():
 
     md = ds[0].metadata()
     md1 = md.override(shortName="msl")
-    fs = FieldList.from_numpy(v1, md1)
+    r = FieldList.from_numpy(v1, md1)
 
-    assert len(fs) == 1
-    assert np.allclose(v1, fs[0].values)
-    assert fs[0].shape == ds[0].shape
-    assert fs[0].metadata("shortName") == "msl"
+    assert len(r) == 1
+    assert np.allclose(v1, r[0].values)
+    assert r[0].shape == ds[0].shape
+    assert r[0].metadata("shortName") == "msl"
+
+    # save to disk
+    tmp = temp_file()
+    r.save(tmp.path)
+    assert os.path.exists(tmp.path)
+    r_tmp = from_source("file", tmp.path)
+    assert len(r_tmp) == 1
+    assert np.allclose(v1, r_tmp[0].values)
+    assert r_tmp[0].shape == ds[0].shape
+    assert r_tmp[0].metadata("shortName") == "msl"
 
 
 def test_numpy_list_grib_multi_field():
@@ -52,6 +64,18 @@ def test_numpy_list_grib_multi_field():
     assert len(r) == 2
     assert np.allclose(v1, r.values)
     for i, f in enumerate(r):
+        assert f.shape == ds[i].shape
+        assert f.metadata("shortName") == "2d", f"shortName {i}"
+        assert f.metadata("name") == "2 metre dewpoint temperature", f"name {i}"
+
+    # save to disk
+    tmp = temp_file()
+    r.save(tmp.path)
+    assert os.path.exists(tmp.path)
+    r_tmp = from_source("file", tmp.path)
+    assert len(r_tmp) == 2
+    assert np.allclose(v1, r_tmp.values)
+    for i, f in enumerate(r_tmp):
         assert f.shape == ds[i].shape
         assert f.metadata("shortName") == "2d", f"shortName {i}"
         assert f.metadata("name") == "2 metre dewpoint temperature", f"name {i}"
