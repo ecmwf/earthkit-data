5e381be505bf477551c8b5c2e12c0ea58cbee661
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-08-09 13:13:01 +0000

    134 add inputs transformer tools for earthkit subpackages (#148)
    * added module_inputs_wrapper and removed unused duplicate script
     
    * possibility to escape wrapping at call

---
+++ tests/utils/dummy_module.py
@@ -0,0 +1,23 @@
+import typing as T
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+dummy_attribute_1 = 1
+dummy_attribute_2 = 2
+
+XR_TYPES = (xr.Dataset, xr.DataArray, xr.Variable)
+
+
+def numpy_mean(
+    a: T.Union[
+        np.ndarray, xr.Dataset, xr.DataArray, xr.Variable, pd.DataFrame, pd.Series
+    ],
+    **kwargs
+):
+    return np.mean(a, **kwargs)
+
+
+def xarray_ones_like(dataarray: T.Union[xr.DataArray, xr.Dataset], **kwargs):
+    return xr.ones_like(dataarray, **kwargs)
