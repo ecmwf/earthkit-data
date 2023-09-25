058d09372d22529676b55c7da1ff9db97a0227ca
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2023-09-25 17:11:12 +0100

    Improve tests

--- tests/sources/test_url.py
+++ tests/sources/test_url.py
@@ -15,7 +15,7 @@ import sys
 import pytest
 
 from earthkit.data import from_source, settings
-from earthkit.data.core.temporary import temp_directory
+from earthkit.data.core.temporary import temp_directory, temp_file
 from earthkit.data.testing import (
     earthkit_file,
     earthkit_remote_test_data_file,
@@ -134,11 +134,14 @@ def test_url_part_file_source():
 
 
 def test_url_netcdf_source_save():
-    ek_url_nc_source = from_source(
+    ds = from_source(
         "url",
         earthkit_remote_test_data_file("examples/test.nc"),
     )
-    ek_url_nc_source.save("temp.nc")
+
+    tmp = temp_file()
+    ds.save(tmp.path)
+    assert os.path.exists(tmp.path)
 
 
 if __name__ == "__main__":
