6845996f0fc886cdc869979a114d3e13a9bf726a
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-10-11 22:25:43 +0100

    consistent pandas_read_csv_kwargs for file and CDS sources (#211)
    * Providing different defaults to different sources is confusing
    
    * need to specify compression method used by source
    
    * read_csv kwargs consistent for cds and file sources
    
    * improved kwarg handling of read_csv
    
    * test demonstrating that default parse_date kwargs is not a good idea
    
    * Tests for other formats
    
    * Compression removed from CSV.to_pandas kwargs, special treatment not required
    
    ---------
    
    Co-authored-by: Sandor Kertesz <Sandor.Kertesz@ecmwf.int>

--- tests/sources/test_cds.py
+++ tests/sources/test_cds.py
@@ -102,6 +102,94 @@ def test_cds_netcdf_selection_limited():
     assert len(s) == 9
 
 
+@pytest.mark.long_test
+@pytest.mark.download
+@pytest.mark.skipif(NO_CDS, reason="No access to CDS")
+def test_cds_observation_csv_file_to_pandas_xarray():
+    collection_id = "insitu-observations-gruan-reference-network"
+    request = {
+        "format": "csv-lev.zip",
+        "year": "2006",
+        "month": "05",
+        "variable": ["air_temperature", "altitude"],
+        "day": ["21", "22"],
+    }
+    data_cds = from_source("cds", collection_id, **request)
+    data_file = from_source("file", data_cds.path)
+    assert "report_timestamp" in data_cds.to_pandas().columns
+
+    # Assert consistent behaviour for local and CDS versions
+    assert data_cds.to_pandas().equals(data_file.to_pandas())
+    assert data_cds.to_xarray().equals(data_file.to_xarray())
+
+
+@pytest.mark.long_test
+@pytest.mark.download
+@pytest.mark.skipif(NO_CDS, reason="No access to CDS")
+def test_cds_non_observation_csv_file_to_pandas_xarray():
+    collection_id = "sis-energy-derived-reanalysis"
+    request = {
+        "variable": "wind_power_generation_onshore",
+        "spatial_aggregation": "country_level",
+        "energy_product_type": "energy",
+        "temporal_aggregation": "daily",
+        "format": "zip",
+    }
+    data_cds = from_source("cds", collection_id, **request)
+    assert "Date" in data_cds.to_pandas().columns
+
+    # Assert a consistent behviour for local and remote versions
+    data_file = from_source("file", data_cds.path)
+    assert data_cds.to_pandas().equals(data_file.to_pandas())
+    assert data_cds.to_xarray().equals(data_file.to_xarray())
+
+
+@pytest.mark.long_test
+@pytest.mark.download
+@pytest.mark.skipif(NO_CDS, reason="No access to CDS")
+def test_cds_grib_to_pandas_xarray():
+    collection_id = "reanalysis-era5-single-levels"
+    request = dict(
+        variable=["2t", "msl"],
+        product_type="reanalysis",
+        area=[50, -50, 20, 50],
+        date="2012-12-12/to/2012-12-15",
+        time="12:00",
+    )
+    data_cds = from_source("cds", collection_id, **request)
+
+    # Assert a consistent behviour for local and remote versions
+    data_file = from_source("file", data_cds.path)
+    assert data_cds.to_pandas().equals(data_file.to_pandas())
+    assert data_cds.to_xarray().equals(data_file.to_xarray())
+
+
+@pytest.mark.long_test
+@pytest.mark.download
+@pytest.mark.skipif(NO_CDS, reason="No access to CDS")
+def test_cds_netcdf_to_pandas_xarray():
+    collection_id = "satellite-methane"
+    request = {
+        "format": "zip",
+        "processing_level": "level_2",
+        "variable": "xch4",
+        "sensor_and_algorithm": "merged_emma",
+        "version": "4.4",
+        "year": "2021",
+        "month": "01",
+        "day": "01",
+    }
+
+    data_cds = from_source("cds", collection_id, **request)
+    assert "xch4" in data_cds.to_xarray().data_vars
+
+    # Assert a consistent behviour for local and remote versions
+    data_file = from_source("file", data_cds.path)
+    assert data_cds.to_xarray().equals(data_file.to_xarray())
+    # Implement to_dataframe
+    # assert data_cds.to_pandas().equals(data_file.to_pandas())
+
+
 if __name__ == "__main__":
     from earthkit.data.testing import main
 
