21e24ebd080b7884aa6597d23f6ee4ab5309d592
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-10-12 16:33:29 +0100

    filename detector

--- tests/sources/test_file.py
+++ tests/sources/test_file.py
@@ -33,6 +33,19 @@ def test_file_source_netcdf():
     assert len(s) == 2
 
 
+def test_file_source_netcdf_save():
+    s = from_source("file", earthkit_examples_file("test.nc"))
+    with temp_directory() as tmpdir:
+        # Check file save to assigned filename
+        s.save(os.path.join(tmpdir, "test2.nc"))
+        assert os.path.isfile(os.path.join(tmpdir, "test2.nc"))
+        # Check file can be saved in current dir with detected filename:
+        here =os.curdir
+        os.chdir(tmpdir)
+        s.save()
+        assert os.path.isfile("test.nc")
+        os.chdir(here)
+
 def test_file_source_odb():
     s = from_source("file", earthkit_examples_file("test.odb"))
     assert s.path == earthkit_examples_file("test.odb")
