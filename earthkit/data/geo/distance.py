071da54587da0a518c103af649bdf681b194f843
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2023-10-20 14:37:23 +0100

    Feature/multiple reference points nearest haversine (#242)
    * Allow multiple reference points for nearest haversine

--- earthkit/data/geo/distance.py
+++ earthkit/data/geo/distance.py
@@ -87,25 +87,26 @@ def haversine_distance(p1, p2):
     return distance
 
 
-def nearest_point_haversine(ref_point, points):
-    """Find the index of the nearest point to ``ref_point`` in a set of ``points`` using the
+def nearest_point_haversine(ref_points, points):
+    """Find the index of the nearest point to all ``ref_points`` in a set of ``points`` using the
        haversine distance formula.
 
     Parameters
     ----------
-    point_ref: pair of numbers
-        Latitude and longitude coordinate of the reference point (degrees)
+    ref_points: pair of array-like
+        Latitude and longitude coordinates of the reference point (degrees)
     points: pair of array-like
         Locations of the set of points from which the nearest to
-        ``point_ref`` is to be found. The first item specifies the latitudes,
+        ``ref_points`` is to be found. The first item specifies the latitudes,
         the second the longitudes (degrees)
 
     Returns
     -------
-    number
-        Index of the nearest point to ``ref_point` in ``points``.
-    number
-        Distance (m) between ``ref_point`` and the nearest point in ``points``.
+    ndarray
+        Indices of the nearest points to ``ref_points`.
+    ndarray
+        The distance (m) between the ``ref_points`` and the corresponding nearest
+        point in ``points``.
 
     Examples
     --------
@@ -114,14 +115,30 @@ def nearest_point_haversine(ref_point, points):
     >>> p_lat = [44.49, 50.73, 50.1]
     >>> p_lon = [11.34, 7.90, -8.1]
     >>> nearest_point_haversine(p_ref, (p_lat, p_lon))
-    (2, 523115.8314777661)
+    (array([2]), array([523115.83147777]))
+
+    >>> from earthkit.data.geo import nearest_point_haversine
+    >>> p_ref = [(51.45, 41.49, 12.29), (-0.97, 18.34, -17.1)]
+    >>> p_lat = [44.49, 50.73, 50.1]
+    >>> p_lon = [11.34, 7.90, -8.1]
+    >>> nearest_point_haversine(p_ref, (p_lat, p_lon))
+    (array([2, 0, 2]), array([ 523115.83147777,  659558.55282001, 4283987.17429322]))
 
     """
-    if np.asarray(ref_point[0]).size != 1 or np.asarray(ref_point[1]).size != 1:
-        raise ValueError("nearest_point_haversine: ref_point must be a single point")
+    ref_points = np.asarray(ref_points)
+    if ref_points.shape == (2,):
+        ref_points = np.array([[ref_points[0]], [ref_points[1]]])
+    elif len(ref_points.shape) != 2 or ref_points.shape[0] != 2:
+        raise ValueError(
+            f"nearest_point_haversine: ref_point expected shape of (2,), got {ref_points.shape}"
+        )
 
-    distance = haversine_distance(ref_point, points)
-    index = np.nanargmin(distance)
-    if isinstance(index, np.ndarray):
-        return index[0]
-    return (index, distance[index])
+    res_index = []
+    res_distance = []
+    for lat, lon in ref_points.T:
+        distance = haversine_distance((lat, lon), points).flatten()
+        index = np.nanargmin(distance)
+        index = index[0] if isinstance(index, np.ndarray) else index
+        res_index.append(index)
+        res_distance.append(distance[index])
+    return (np.array(res_index), np.array(res_distance))
--- tests/geo/test_geo.py
+++ tests/geo/test_geo.py
@@ -111,18 +111,46 @@ def test_haversine_distance_invalid(p_ref):
         ((-50, 18), 9, 265965.0757389435),
     ],
 )
-def test_haversine_nearest(p_ref, index_ref, dist_ref):
+def test_haversine_nearest_single_ref(p_ref, index_ref, dist_ref):
     lats = np.array([0.0, 0, 0, 0, 90, -90, 48, 48, -48, -48, np.nan])
     lons = np.array([0, 90, -90, 180, 0, 0, 20, -20, -20, 20, 1.0])
 
     index, distance = nearest_point_haversine(p_ref, (lats, lons))
-    assert index == index_ref, p_ref
-    assert np.isclose(distance, dist_ref)
+    assert np.allclose(index_ref, index)
+    assert np.allclose(distance, dist_ref)
+
+
+def test_haversine_nearest_multi_ref_single_point():
+    p_ref = [(15, 44, 44), (22, 10, -10)]
+
+    index_ref = np.array([0, 0, 0])
+    dist_ref = np.array([3669564.47380201, 1026629.42232992, 2492883.80828643])
+
+    lats = 48
+    lons = 22
+
+    index, distance = nearest_point_haversine(p_ref, (lats, lons))
+    assert np.allclose(index, index_ref)
+    assert np.allclose(distance, dist_ref)
+
+
+def test_haversine_nearest_multi_ref_multi_points():
+    p_ref = [(15, 44, 44), (22, 10, -10)]
+
+    index_ref = np.array([0, 6, 7])
+    dist_ref = np.array([2937383.915942687, 890348.1323086087, 890348.1323086087])
+
+    lats = np.array([0.0, 0, 0, 0, 90, -90, 48, 48, -48, -48, np.nan])
+    lons = np.array([0, 90, -90, 180, 0, 0, 20, -20, -20, 20, 1.0])
+
+    index, distance = nearest_point_haversine(p_ref, (lats, lons))
+    assert np.allclose(index, index_ref)
+    assert np.allclose(distance, dist_ref)
 
 
 def test_haversine_nearest_invalid():
-    # checks: p_ref must be a single point
-    p_ref = ([15, 21], [22, 7])
+    # checks: p_ref must have a (2,) shape
+    p_ref = ([15, 21], [22, 7], [44, 12])
     p = ([0.0, 0], [0, 90])
 
     with pytest.raises(ValueError):
