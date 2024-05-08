da96779fa14d15a7e105b51809d2dd6639ef9693
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2024-05-08 12:42:13 +0100

    Use pyproject.toml and src root dir

--- tests/sources/test_url.py
+++ tests/sources/test_url.py
@@ -17,6 +17,7 @@ import pytest
 from earthkit.data import from_source, settings
 from earthkit.data.core.temporary import temp_directory, temp_file
 from earthkit.data.testing import (
+    earthkit_examples_file,
     earthkit_file,
     earthkit_remote_test_data_file,
     network_off,
@@ -28,7 +29,8 @@ from earthkit.data.testing import (
     reason="file:// not working on Windows yet",
 )
 def test_url_file_source():
-    filename = os.path.abspath(earthkit_file("docs/examples/test.nc"))
+    # filename = os.path.abspath(earthkit_file("docs/examples/test.nc"))
+    filename = os.path.abspath(earthkit_examples_file("test.nc"))
     s = from_source("url", f"file://{filename}")
     assert len(s) == 2
 
