18721bb5d1a7931d6b5c7703734434d29d37e5f3
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2024-09-07 09:53:27 +0100

    Fix memory leak in metadata cache

--- src/earthkit/data/readers/grib/index/__init__.py
+++ src/earthkit/data/readers/grib/index/__init__.py
@@ -309,9 +309,6 @@ class GribHandleManager:
     def _handle_created(self):
         self.handle_create_count += 1
 
-    def _make_metadata_cache(self):
-        return dict()
-
     def diag(self):
         r = defaultdict(int)
         r["grib_handle_policy"] = self.policy
