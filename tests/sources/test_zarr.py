498295797620cff7bf2ae4564bceae1678e97360
oisin-m <oisinmmm@gmail.com>
2025-06-02 18:35:16 +0200

    skip zarr test if zarr not installed

--- tests/sources/test_zarr.py
+++ tests/sources/test_zarr.py
@@ -1,8 +1,18 @@
+import importlib.util
+
+import pytest
+
 from earthkit.data import from_source
 from earthkit.data.readers.netcdf.field import XArrayField
 from earthkit.data.testing import earthkit_test_data_file
 
+if importlib.util.find_spec("zarr") is not None:
+    NO_ZARR = False
+else:
+    NO_ZARR = True
 
+
+@pytest.mark.skipif(NO_ZARR, reason="Zarr not installed")
 def test_zarr_source():
     ds = from_source("xarray-zarr", earthkit_test_data_file("test_zarr/"))
     assert len(ds) == 4
