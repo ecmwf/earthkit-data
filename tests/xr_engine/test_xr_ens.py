27e6f6387d332e7f1d8bae2783631e7d0126ef91
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2025-06-10 14:33:53 +0100

    Handle step range

--- tests/xr_engine/test_xr_ens.py
+++ tests/xr_engine/test_xr_ens.py
@@ -16,7 +16,6 @@ import pytest
 
 from earthkit.data import from_source
 from earthkit.data.testing import earthkit_remote_test_data_file
-from earthkit.data.utils.xarray.profile import PROFILE_CONF
 
 here = os.path.dirname(__file__)
 sys.path.insert(0, here)
