d08f6f0dfae526b25db6538a002cda1854aa64d9
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2025-07-01 15:18:42 +0100

    Write lod fields to grib

--- tests/list_of_dicts/test_lod_to_grib.py
+++ tests/list_of_dicts/test_lod_to_grib.py
@@ -22,13 +22,22 @@ def test_lod_to_grib_1():
 
     d = ({"param": "t", "levelist": 500, "date": 20010101, "time": 1200, "step": 6, "values": data},)
     ds = from_source("list-of-dicts", d)
-
-    print(ds[0].shape)
+    assert ds[0].shape == (181, 360)
 
     with temp_file() as path:
         ds.to_target("file", path, encoder="grib")
 
-        from_source("file", path)
+        ds_r = from_source("file", path)
+        assert len(ds_r) == 1
+
+        f = ds_r[0]
+        assert f.shape == (181, 360)
+        assert f.metadata("param") == "t"
+        assert f.metadata("levelist") == 500
+        assert f.metadata("date") == 20010101
+        assert f.metadata("time") == 1200
+        assert f.metadata("step") == 6
+        np.allclose(f.values, data.flatten())
 
 
 def test_lod_to_grib_2():
@@ -46,6 +55,7 @@ def test_lod_to_grib_2():
         },
     )
     ds = from_source("list-of-dicts", d)
+    assert ds[0].shape == (181, 360)
 
     with temp_file() as path:
         # This should fail because of the veryBadKey
@@ -54,6 +64,18 @@ def test_lod_to_grib_2():
 
     with temp_file() as path:
         md = {"param": "t", "levelist": 500, "date": 20010101, "time": 1200, "step": 6}
+
+        # This does not fail because we do not use any metadata from data
         ds.to_target("file", path, encoder="grib", metadata=md, use_metadata_from_data=False)
 
-        from_source("file", path)
+        ds_r = from_source("file", path)
+        assert len(ds_r) == 1
+
+        f = ds_r[0]
+        assert f.shape == (181, 360)
+        assert f.metadata("param") == "t"
+        assert f.metadata("levelist") == 500
+        assert f.metadata("date") == 20010101
+        assert f.metadata("time") == 1200
+        assert f.metadata("step") == 6
+        np.allclose(f.values, data.flatten())
