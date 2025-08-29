c4ef88a71440cf9eacf9c79820568f873ef12667
Pawel Wolff <pawel.wolff@ecmwf.int>
2025-08-29 17:02:48 +0200

    .group in XArrayInputFieldList now uses remapping, like .sel does; tests added

--- tests/xr_engine/test_xr_remapping.py
+++ tests/xr_engine/test_xr_remapping.py
@@ -24,11 +24,14 @@ from xr_engine_fixtures import compare_dims  # noqa: E402
 
 
 @pytest.mark.cache
-def test_xr_remapping_1():
+@pytest.mark.parametrize("lazy_load", [True, False])
+def test_xr_remapping_1(lazy_load):
     ds0 = from_source(
         "url", earthkit_remote_test_data_file("test-data/xr_engine/level/mixed_pl_ml_small.grib")
     )
-    ds = ds0.to_xarray(variable_key="_k", remapping={"_k": "{param}_{levelist}_{levtype}"})
+    ds = ds0.to_xarray(
+        variable_key="_k", remapping={"_k": "{param}_{levelist}_{levtype}"}, lazy_load=lazy_load
+    )
 
     data_vars = ["t_137_ml", "t_500_pl", "t_700_pl", "t_90_ml", "u_137_ml", "u_500_pl", "u_700_pl", "u_90_ml"]
     assert [v for v in ds.data_vars] == data_vars
@@ -38,6 +41,7 @@ def test_xr_remapping_1():
 
 
 @pytest.mark.cache
+@pytest.mark.parametrize("lazy_load", [True, False])
 @pytest.mark.parametrize(
     "kwargs,coords,dims",
     [
@@ -74,9 +78,9 @@ def test_xr_remapping_1():
         ),
     ],
 )
-def test_xr_remapping_2(kwargs, coords, dims):
+def test_xr_remapping_2(lazy_load, kwargs, coords, dims):
     ds0 = from_source("url", earthkit_remote_test_data_file("test-data/xr_engine/level/pl_small.grib"))
-    ds = ds0.to_xarray(**kwargs)
+    ds = ds0.to_xarray(lazy_load=lazy_load, **kwargs)
 
     data_vars = ["r", "t"]
 
