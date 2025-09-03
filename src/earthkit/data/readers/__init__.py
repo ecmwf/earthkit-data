ecmwf/earthkit-data

commit 4a831847f8fd13f59551eecbc4748221da82fb28

Fixes serialisation issue #788 (#791)
* Fixed by keeping a ref to Reader._source._kwargs along the weakref to Reader._source

src/earthkit/data/readers/__init__.py
@@ -33,6 +33,7 @@ class Reader(Base, os.PathLike, metaclass=ReaderMeta):
     def __init__(self, source, path):
         LOG.debug("Reader for %s is %s", path, self.__class__.__name__)
 
+        self._source_kwargs = source._kwargs
         self._source = weakref.ref(source)
         self.path = path
         self.source_filename = self.source.source_filename

src/earthkit/data/readers/directory.py
@@ -79,7 +79,7 @@ class DirectoryReader(Reader):
                 merger=self.merger,
                 stream=self.stream,
                 parts=self.parts,
-                **self.source._kwargs,
+                **self._source_kwargs,
             )
 
         return from_source(
@@ -92,7 +92,7 @@ class DirectoryReader(Reader):
                     merger=self.merger,
                     stream=self.stream,
                     parts=self.parts,
-                    **self.source._kwargs,
+                    **self._source_kwargs,
                 )
                 for path in sorted(self._content)
             ],

src/earthkit/data/readers/grib/file.py
@@ -50,7 +50,7 @@ class GRIBReader(GribFieldListInOneFile, Reader):
         from earthkit.data.core.config import CONFIG
 
         policy = CONFIG.get("grib-file-serialisation-policy")
-        r = {"serialisation_policy": policy, "kwargs": self.source._kwargs}
+        r = {"serialisation_policy": policy, "kwargs": self._source_kwargs}
 
         if policy == "path":
             r["path"] = self.path

docs/examples/xarray_engine_chunks.ipynb
@@ -40,7 +40,7 @@
     "tags": []
    },
    "source": [
-    "First, we get 2m temperature data for a whole year on a low resolution regular latitude-longitude grid. It contains 2 fields per day (at 0 and 12 UTC). This data obviously fit into memory, so only used for demonstration purposes."
+    "First, we get 2m temperature data for a whole year on a low resolution regular latitude-longitude grid. It contains 2 fields per day (at 0 and 6 UTC). This data obviously fit into memory, so only used for demonstration purposes."
    ]
   },
   {

tests/documentation/test_notebooks.py
@@ -41,7 +41,7 @@ SKIP = [
     "shapefile.ipynb",
     "grib_to_xarray.ipynb",
     "grib_to_fdb_target.ipynb",
-    "xarray_engine.ipynb",
+    "xarray_engine_chunks_on_dask_cluster.ipynb",
     "xarray_cupy.ipynb",
     "netcdf_opendap.ipynb",
 ]

commit 2ade36718d8371abb7a070f0bd63c606cd473d99

fixes the regression caused by PR #791

src/earthkit/data/readers/__init__.py
@@ -32,8 +32,6 @@ class Reader(Base, os.PathLike, metaclass=ReaderMeta):
 
     def __init__(self, source, path):
         LOG.debug("Reader for %s is %s", path, self.__class__.__name__)
-
-        self._source_kwargs = source._kwargs
         self._source = weakref.ref(source)
         self.path = path
         self.source_filename = self.source.source_filename

src/earthkit/data/readers/directory.py
@@ -47,6 +47,7 @@ class DirectoryReader(Reader):
     def __init__(self, source, path):
         super().__init__(source, path)
         self._content = []
+        self._source_kwargs = source._kwargs
 
         filter = make_file_filter(self.filter, self.path)
 

src/earthkit/data/readers/grib/file.py
@@ -35,6 +35,7 @@ class GRIBReader(GribFieldListInOneFile, Reader):
 
         Reader.__init__(self, source, path)
         GribFieldListInOneFile.__init__(self, path, parts=parts, positions=positions, **_kwargs)
+        self._source_kwargs = source._kwargs
 
     def __repr__(self):
         return "GRIBReader(%s)" % (self.path,)