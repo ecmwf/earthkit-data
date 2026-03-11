6871e222e93de5c3d27ea9441b36b60c7f42246b
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2026-03-11 19:58:51 +0000

    Update docs style

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ source_suffix = ".rst"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -96,7 +96,51 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["style.css"]
 
-html_logo = "https://github.com/ecmwf/logos/raw/refs/heads/main/logos/earthkit/earthkit-data-dark.svg"
+# html_logo = "https://github.com/ecmwf/logos/raw/refs/heads/main/logos/earthkit/earthkit-data-dark.svg"
+
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-sidebar-background": "#131320",
+        # "color-background-primary": "", # leave as default to avoid overriding the light theme background
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "dark_css_variables": {
+        "color-sidebar-background": "#131320",
+        "color-background-primary": "#131320",
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "light_logo": "earthkit-data-light.svg",
+    "dark_logo": "earthkit-data-dark.svg",
+    "source_repository": "https://github.com/ecmwf/earthkit-data/",
+    # "source_branch": source_branch,
+    "source_directory": "docs/source",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/ecmwf/earthkit-data",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
+
 
 xref_links = {
     "botocore": ("botocore", "https://botocore.amazonaws.com/v1/documentation/api/latest/index.html"),
