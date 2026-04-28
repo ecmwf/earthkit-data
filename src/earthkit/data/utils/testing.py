f3c2ab6b2952af2e2ddd77727663d70b9ec5166e
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2026-04-28 13:14:10 +0100

    Fix failing netcdf test (#976)

--- src/earthkit/data/utils/testing.py
+++ src/earthkit/data/utils/testing.py
@@ -221,7 +221,7 @@ def load_nc_or_xr_source(path, mode):
     else:
         import xarray
 
-        return from_object(xarray.open_dataset(path)).to_fieldlist()
+        return from_object(xarray.open_dataset(path, decode_timedelta=True)).to_fieldlist()
 
 
 class ArrayBackend:
