a8d48bc3972cf5a56dce843c33c98fbe5093d9f7
Pawel Wolff <97901989+pawel-wolff@users.noreply.github.com>
2026-06-01 12:21:24 +0200

    xr_engine profile-defined variable_attrs changed from GRIB-specific to format-agnostic (#1006)
    A related test improved
    
    Documentation on xr_engine `*_attrs` corrected and expanded

--- src/earthkit/data/indexing/xarray.py
+++ src/earthkit/data/indexing/xarray.py
@@ -286,15 +286,35 @@ class XarrayMixIn:
                 The following attributes are supported:
 
                 - str: Name of the attribute used as a metadata key to generate the value of
-                  the attribute. Can also be specified by prefixing with "key=" (e.g. "key=level").
+                  the attribute. Can also be specified by prefixing with "key=" (e.g. "key=vertical.level").
                   When prefixed with "namespace=" it specifies a metadata namespace
                   (e.g. "namespace=parameter"), which will be added as a dict to the attribute.
-                - callable: A callable that takes a Metadata object and returns a dict of attributes
+                - callable: A callable that takes a Field object and returns a dict of attributes, e.g.:
+
+                  .. code-block:: python
+
+                    def rounded_wavelength(field):
+                        wl = field.get("metadata.wavelength")
+                        if wl is not None:
+                            return {"wavelength": round(wl)}
+                        else:
+                            return {}
+
                 - dict: A dictionary of attributes with the keys as the attribute names.
-                  If the value is a callable it takes the attribute name and a Metadata object and
-                  returns the value of the attribute. A str value prefixed with "key=" or
-                  "namespace=" is interpreted as explained above. Any other values are used as the
-                  pre-defined value for the attribute.
+                  If the value is a callable it takes the attribute name and a Field object and
+                  returns the value of the attribute, e.g.:
+
+                  .. code-block:: python
+
+                    def ensure_rounded(key, field):
+                        val = field.get(key)
+                        try:
+                            return round(val)
+                        except Exception:
+                            return val
+
+                  A str value prefixed with "key=" or "namespace=" is interpreted as explained above.
+                  Any other values are used as the pre-defined value for the attribute.
             * variable_attrs: str, number, callable, dict or list of these, None
                 Variable attribute or attributes. For the allowed values see ``attrs``. Its
                 default value (None) expands to [] unless the ``profile`` overwrites it.
