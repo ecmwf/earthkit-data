743172c8ef83f22bb26faea838c243f168a237cd
oisin-m <oisinmmm@gmail.com>
2026-08-27 17:02:54 +0200

    fix: imports and tests

--- src/earthkit/data/utils/testing.py
+++ src/earthkit/data/utils/testing.py
@@ -23,8 +23,6 @@ from earthkit.utils.array import convert as convert_array
 from earthkit.utils.array.testing import NAMESPACE_DEVICES
 
 from earthkit.data import from_object, from_source
-from earthkit.data.readers.text import TextReader
-from earthkit.data.sources.empty import EmptySource
 from earthkit.data.sources.mars import StandaloneMARSClient
 
 LOG = logging.getLogger(__name__)
@@ -183,39 +181,6 @@ NO_IRIS = not (modules_installed("iris") and modules_installed("ncdata"))
 
 def MISSING(*modules):
     return not modules_installed(*modules)
-
-
-UNSAFE_SAMPLES_URL = "https://github.com/jwilk/traversal-archives/releases/download/0"
-
-
-def empty(ds):
-    LOG.debug("%s", ds)
-    assert isinstance(ds, EmptySource)
-    assert len(ds) == 0
-
-
-def text(ds):
-    LOG.debug("%s", ds)
-    assert isinstance(ds._reader, TextReader)
-
-
-UNSAFE_SAMPLES = (
-    ("absolute1", empty),
-    ("absolute2", empty),
-    ("relative0", empty),
-    ("relative2", empty),
-    ("symlink", text),
-    ("dirsymlink", text),
-    ("dirsymlink2a", text),
-    ("dirsymlink2b", text),
-)
-
-
-def check_unsafe_archives(extension):
-    for archive, check in UNSAFE_SAMPLES:
-        LOG.debug("%s.%s", archive, extension)
-        ds = from_source("url", f"{UNSAFE_SAMPLES_URL}/{archive}{extension}")
-        check(ds)
 
 
 def load_nc_or_xr_source(path, mode):
