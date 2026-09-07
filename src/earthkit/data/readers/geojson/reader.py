4eaa6caca7c4d9d43b22f79214675bf7870c9272
Oisin-M <60450429+Oisin-M@users.noreply.github.com>
2026-09-07 14:08:13 +0200

    chore: renaming (#1134)
    * chore: renaming

--- src/earthkit/data/readers/geojson/reader.py
+++ src/earthkit/data/readers/geojson/reader.py
@@ -12,13 +12,13 @@
 
 from earthkit.data.sources import Source
 
-from .core import GeoJsonReaderBase
+from .core import GeoJSONReaderBase
 
 
-class GeojsonReader(Source, GeoJsonReaderBase):
+class GeoJSONReader(Source, GeoJSONReaderBase):
     def __init__(self, source, path):
         self._ori_source = source
-        GeoJsonReaderBase.__init__(self, source, path)
+        GeoJSONReaderBase.__init__(self, source, path)
 
     def to_pandas(self, **kwargs):
         # TODO: handle multiple paths
@@ -32,9 +32,9 @@ class GeojsonReader(Source, GeoJsonReaderBase):
         return self.to_pandas(**kwargs).to_xarray()
 
     def to_featurelist(self, *args, **kwargs):
-        from .file import GeoJsonList
+        from .file import GeoJSONList
 
-        return GeoJsonList(self.path)
+        return GeoJSONList(self.path)
 
     def mutate_source(self):
         # A Geojson is a source itself
@@ -54,9 +54,9 @@ class GeojsonReader(Source, GeoJsonReaderBase):
         return geo_df.set_index(np.arange(len(geo_df)))
 
     def to_data_object(self):
-        from earthkit.data.data.geojson import GeoJsonData
+        from earthkit.data.data.geojson import GeoJSONData
 
-        return GeoJsonData(self)
+        return GeoJSONData(self)
 
     def _encode_default(self, encoder, *args, **kwargs):
         return None
